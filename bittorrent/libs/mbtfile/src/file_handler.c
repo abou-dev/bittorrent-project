3924796002f8be05b32889a4abc97524abb1deff
Aboubakar Saly <aboubakar.saly.etu@esmt.sn>
2024-11-28 21:15:47 +0100

    mbtfile

--- bittorrent/libs/mbtfile/src/file_handler.c
+++ bittorrent/libs/mbtfile/src/file_handler.c
@@ -1,23 +1,25 @@
 #include "mbt/file/file_handler.h"
-#include "mbt/file/piece.h"
 
-#include <mbt/utils/str.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <mbt/be/torrent.h>
-
-#include <stdlib.h>
-#include <string.h>
+#include <mbt/utils/str.h>
 #include <openssl/sha.h>
 #include <stdio.h>
-#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
-#include <errno.h>
 
-struct mbt_file_handler {
+#include "mbt/file/piece.h"
+
+struct mbt_file_handler
+{
     struct mbt_torrent *torrent;
     struct mbt_piece *pieces;
     size_t nb_pieces;
-    const char *pieces_hash; // Hash des pièces (ne pas libérer)
+    const char *pieces_hash;
 };
+
 struct mbt_file_handler *mbt_file_handler_init(struct mbt_torrent *torrent)
 {
     if (torrent == NULL)
@@ -32,7 +34,8 @@ struct mbt_file_handler *mbt_file_handler_init(struct mbt_torrent *torrent)
     fh->nb_pieces = mbt_torrent_get_nb_pieces(torrent);
 
     fh->pieces = malloc(sizeof(struct mbt_piece) * fh->nb_pieces);
-    if (fh->pieces == NULL) {
+    if (fh->pieces == NULL)
+    {
         free(fh);
         return NULL;
     }
@@ -40,14 +43,26 @@ struct mbt_file_handler *mbt_file_handler_init(struct mbt_torrent *torrent)
 
     fh->pieces_hash = mbt_torrent_get_pieces_hash(torrent);
 
-    for (size_t i = 0; i < fh->nb_pieces; i++) {
+    size_t total_size = mbt_torrent_get_total_length(torrent);
+    size_t piece_length = mbt_torrent_get_piece_length(torrent);
+
+    for (size_t i = 0; i < fh->nb_pieces; i++)
+    {
         struct mbt_piece *p = &fh->pieces[i];
 
-        size_t piece_size = mbt_torrent_get_piece_size(torrent, i);
+        size_t piece_size = piece_length;
+        if (i == fh->nb_pieces - 1)
+        {
+            size_t remaining = total_size - (piece_length * i);
+            if (remaining < piece_length)
+                piece_size = remaining;
+        }
         p->size = piece_size;
         p->data = malloc(piece_size);
-        if (p->data == NULL) {
-            for (size_t j = 0; j < i; j++) {
+        if (p->data == NULL)
+        {
+            for (size_t j = 0; j < i; j++)
+            {
                 mbt_piece_dtor(&fh->pieces[j]);
             }
             free(fh->pieces);
@@ -58,9 +73,11 @@ struct mbt_file_handler *mbt_file_handler_init(struct mbt_torrent *torrent)
 
         p->nb_blocks = (piece_size + MBT_BLOCK_SIZE - 1) / MBT_BLOCK_SIZE;
         p->received_blocks = malloc(sizeof(bool) * p->nb_blocks);
-        if (p->received_blocks == NULL) {
+        if (p->received_blocks == NULL)
+        {
             free(p->data);
-            for (size_t j = 0; j < i; j++) {
+            for (size_t j = 0; j < i; j++)
+            {
                 mbt_piece_dtor(&fh->pieces[j]);
             }
             free(fh->pieces);
@@ -72,17 +89,19 @@ struct mbt_file_handler *mbt_file_handler_init(struct mbt_torrent *torrent)
 
     return fh;
 }
+
 void mbt_file_handler_free(struct mbt_file_handler *fh)
 {
     if (fh == NULL)
         return;
-    for (size_t i = 0; i < fh->nb_pieces; i++) {
+    for (size_t i = 0; i < fh->nb_pieces; i++)
+    {
         mbt_piece_dtor(&fh->pieces[i]);
     }
     free(fh->pieces);
-    // Ne pas libérer fh->pieces_hash car il appartient au torrent
     free(fh);
 }
+
 size_t mbt_file_handler_get_nb_pieces(struct mbt_file_handler *fh)
 {
     if (fh == NULL)
@@ -93,7 +112,7 @@ size_t mbt_file_handler_get_nb_pieces(struct mbt_file_handler *fh)
 struct mbt_cview mbt_file_handler_get_name(struct mbt_file_handler *fh)
 {
     if (fh == NULL)
-        return (struct mbt_cview){NULL, 0};
+        return (struct mbt_cview){ NULL, 0 };
     return mbt_torrent_get_name(fh->torrent);
 }
 
@@ -117,4 +136,3 @@ const char *mbt_file_handler_get_pieces_hash(struct mbt_file_handler *fh)
         return NULL;
     return fh->pieces_hash;
 }
-
--- bittorrent/libs/mbtfile/src/piece.c
+++ bittorrent/libs/mbtfile/src/piece.c
@@ -1,23 +1,25 @@
 #include "mbt/file/piece.h"
-#include "mbt/file/file_handler.h"
 
-#include <mbt/utils/str.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <mbt/be/torrent.h>
-
+#include <mbt/utils/str.h>
 #include <openssl/sha.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-#include <fcntl.h>
 #include <unistd.h>
-#include <errno.h>
 
-struct mbt_piece {
+#include "mbt/file/file_handler.h"
+
+struct mbt_piece
+{
     size_t size;
     char *data;
     size_t nb_blocks;
     bool *received_blocks;
 };
+
 void mbt_piece_dtor(struct mbt_piece *p)
 {
     if (p == NULL)
@@ -27,18 +29,21 @@ void mbt_piece_dtor(struct mbt_piece *p)
     p->data = NULL;
     p->received_blocks = NULL;
 }
+
 size_t mbt_piece_get_nb_blocks(struct mbt_file_handler *fh, size_t piece_index)
 {
     if (fh == NULL || piece_index >= fh->nb_pieces)
         return 0;
     return fh->pieces[piece_index].nb_blocks;
 }
+
 const char *mbt_piece_get_data(struct mbt_file_handler *fh, size_t piece_index)
 {
     if (fh == NULL || piece_index >= fh->nb_pieces)
         return NULL;
     return fh->pieces[piece_index].data;
 }
+
 void mbt_piece_set_data(struct mbt_file_handler *fh, size_t piece_index,
                         const char *data, size_t size)
 {
@@ -49,6 +54,7 @@ void mbt_piece_set_data(struct mbt_file_handler *fh, size_t piece_index,
         size = p->size;
     memcpy(p->data, data, size);
 }
+
 bool mbt_piece_block_is_received(struct mbt_file_handler *fh,
                                  size_t piece_index, size_t block_index)
 {
@@ -59,6 +65,7 @@ bool mbt_piece_block_is_received(struct mbt_file_handler *fh,
         return false;
     return p->received_blocks[block_index];
 }
+
 void mbt_piece_block_set_received(struct mbt_file_handler *fh,
                                   size_t piece_index, size_t block_index,
                                   bool received)
@@ -70,19 +77,22 @@ void mbt_piece_block_set_received(struct mbt_file_handler *fh,
         return;
     p->received_blocks[block_index] = received;
 }
+
 bool mbt_compare_hashes(struct mbt_file_handler *fh, size_t piece_index,
-                        const char *received_data)
+                        const char *data, size_t size)
 {
-    if (fh == NULL || piece_index >= fh->nb_pieces || received_data == NULL)
+    if (fh == NULL || piece_index >= fh->nb_pieces || data == NULL)
         return false;
-    struct mbt_piece *p = &fh->pieces[piece_index];
 
     unsigned char computed_hash[SHA_DIGEST_LENGTH];
-    SHA1((const unsigned char *)received_data, p->size, computed_hash);
+    SHA1((const unsigned char *)data, size, computed_hash);
 
-    const unsigned char *expected_hash = (const unsigned char *)(fh->pieces_hash + piece_index * SHA_DIGEST_LENGTH);
+    const unsigned char *expected_hash =
+        (const unsigned char *)(fh->pieces_hash
+                                + piece_index * SHA_DIGEST_LENGTH);
     return (memcmp(computed_hash, expected_hash, SHA_DIGEST_LENGTH) == 0);
 }
+
 enum mbt_piece_status mbt_piece_check(struct mbt_file_handler *fh,
                                       size_t piece_index)
 {
@@ -90,16 +100,18 @@ enum mbt_piece_status mbt_piece_check(struct mbt_file_handler *fh,
         return MBT_PIECE_INVALID;
     struct mbt_piece *p = &fh->pieces[piece_index];
 
-    for (size_t i = 0; i < p->nb_blocks; i++) {
+    for (size_t i = 0; i < p->nb_blocks; i++)
+    {
         if (!p->received_blocks[i])
             return MBT_PIECE_DOWNLOADING;
     }
 
-    if (mbt_compare_hashes(fh, piece_index, p->data))
+    if (mbt_compare_hashes(fh, piece_index, p->data, p->size))
         return MBT_PIECE_VALID;
     else
         return MBT_PIECE_INVALID;
 }
+
 bool mbt_piece_write(struct mbt_file_handler *fh, size_t piece_index)
 {
     if (fh == NULL || piece_index >= fh->nb_pieces)
@@ -107,49 +119,89 @@ bool mbt_piece_write(struct mbt_file_handler *fh, size_t piece_index)
     struct mbt_piece *p = &fh->pieces[piece_index];
 
     size_t piece_length = mbt_torrent_get_piece_length(fh->torrent);
-    size_t piece_offset = piece_index * piece_length;
+    size_t total_size = mbt_torrent_get_total_length(fh->torrent);
 
-    size_t cumulative_size = 0;
+    size_t piece_offset = piece_index * piece_length;
+    if (piece_offset + p->size > total_size)
+        p->size = total_size - piece_offset;
+
+    size_t remaining_size = p->size;
+    size_t data_offset = 0;
+
     size_t nb_files = mbt_torrent_get_nb_files(fh->torrent);
-    for (size_t i = 0; i < nb_files; i++) {
+    size_t cumulative_size = 0;
+
+    for (size_t i = 0; i < nb_files && remaining_size > 0; i++)
+    {
         struct mbt_file_info file_info;
         mbt_torrent_get_file_info(fh->torrent, i, &file_info);
-        if (piece_offset < cumulative_size + file_info.length) {
-            size_t file_offset = piece_offset - cumulative_size;
+        size_t file_start = cumulative_size;
+        size_t file_end = cumulative_size + file_info.length;
 
-            size_t write_size = p->size;
+        if (piece_offset < file_end && piece_offset + p->size > file_start)
+        {
+            size_t file_offset = 0;
+            if (piece_offset > file_start)
+                file_offset = piece_offset - file_start;
+
+            size_t write_size = remaining_size;
             if (file_offset + write_size > file_info.length)
                 write_size = file_info.length - file_offset;
 
-            int fd = open(file_info.path.data, O_WRONLY | O_CREAT, 0644);
-            if (fd == -1) {
+            mbt_str_t path_str = mbt_str_from_cview(file_info.path);
+            char *dir_path = strdup(path_str.data);
+            char *last_slash = strrchr(dir_path, '/');
+            if (last_slash)
+            {
+                *last_slash = '\0';
+                char mkdir_cmd[512];
+                snprintf(mkdir_cmd, sizeof(mkdir_cmd), "mkdir -p \"%s\"",
+                         dir_path);
+                system(mkdir_cmd);
+            }
+            free(dir_path);
+            mbt_str_dtor(&path_str);
+
+            mbt_str_t file_path = mbt_str_from_cview(file_info.path);
+            int fd = open(file_path.data, O_WRONLY | O_CREAT, 0644);
+            mbt_str_dtor(&file_path);
+
+            if (fd == -1)
+            {
                 perror("open failed");
                 return false;
             }
 
-            // Se positionner au bon offset
-            if (lseek(fd, file_offset, SEEK_SET) == (off_t)-1) {
+            if (lseek(fd, file_offset, SEEK_SET) == (off_t)-1)
+            {
                 perror("lseek failed");
                 close(fd);
                 return false;
             }
 
-            // Écrire les données
-            ssize_t written = write(fd, p->data, write_size);
-            if (written != (ssize_t)write_size) {
+            ssize_t written = write(fd, p->data + data_offset, write_size);
+            if (written != (ssize_t)write_size)
+            {
                 perror("write failed");
                 close(fd);
                 return false;
             }
 
             close(fd);
-            return true;
+
+            remaining_size -= write_size;
+            data_offset += write_size;
+            piece_offset += write_size;
         }
+
         cumulative_size += file_info.length;
     }
 
-    // La pièce n'appartient à aucun fichier (ne devrait pas arriver)
-    fprintf(stderr, "Piece index %zu does not belong to any file.\n", piece_index);
-    return false;
-}
+    if (remaining_size > 0)
+    {
+        fprintf(stderr, "Could not write entire piece %zu.\n", piece_index);
+        return false;
+    }
 
+    return true;
+}
